89b563fd610011225cb8e492f373134ceae2518b
The-Rul <raaulgs@gmail.com>
2023-02-22 21:00:39 +0100

    h2_ejerc5 version con clases

---
+++ Ud07/src/h2_ejer5_version2/Main.java
@@ -0,0 +1,19 @@
+package h2_ejer5_version2;
+
+public class Main {
+	public static void main(String[] args) {
+
+		int ventanillas = 5; //numero ventanillas
+		int t[][] = {{10,20,30},{60,25,15}};//Para parametrizar y poder hacer otras simulaciones
+		
+		Oficina ofi = new Oficina(ventanillas);
+		
+		for (int i = 0; i < 300; i++) {
+			
+		}
+		
+		
+		
+		
+	}
+}
